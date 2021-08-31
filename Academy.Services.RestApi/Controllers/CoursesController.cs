d3ba173500b968f9383d9246ea5682eddec68722
Arash.Sardari6@outlook.com <Arash.Sardari6@outlook.com>
2021-08-31 20:24:09 +0430

    before finish 5 - 1

---
+++ Academy.Services.RestApi/Controllers/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Application.Contract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academy.Services.RestApi.Controllers
+{
+   
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoursesController : Controller
+    {
+        [HttpGet]
+        public List<CourseDto> Get()
+        {
+            return new List<CourseDto>()
+            {
+                new CourseDto() {Id = 1, Title = "Android"},
+                new CourseDto() {Id = 2, Title = "Ios"},
+                new CourseDto() {Id = 3, Title = "Advance Android"}
+            };
+        }   
+    }
+}
---
+++ Application.Contract/CourseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Contract
+{
+    public class CourseDto
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public CourseCategoryDto CategoryDto { get; set; }
+    }
+}
